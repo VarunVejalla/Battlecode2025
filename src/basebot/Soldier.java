11e1092d5081579e0bf7546da7f1265714ae351e
Reevu Adakroy <reevu.adakroy@gmail.com>
2025-01-08 13:33:42 -0500

    Refresh .gitignore rules

--- src/basebot/Soldier.java
+++ src/basebot/Soldier.java
@@ -11,15 +11,15 @@ public class Soldier extends Bunny {
         super.run();
 
         // TODO
-//        if (low on paint) {
-//            go towards nearest friendly tower
-//        }
+        // if (low on paint) {
+        // go towards nearest friendly tower
+        // }
 
         // Sense information about all visible nearby tiles.
         MapInfo[] nearbyTiles = rc.senseNearbyMapInfos();
         MapInfo curRuin = null;
-        for (MapInfo tile : nearbyTiles){
-            if (tile.hasRuin()){
+        for (MapInfo tile : nearbyTiles) {
+            if (tile.hasRuin()) {
                 curRuin = tile;
                 break;
             }
@@ -44,64 +44,62 @@ public class Soldier extends Bunny {
                     rc.markTowerPattern(UnitType.LEVEL_ONE_PAINT_TOWER, curRuin.getMapLocation());
                 }
             }
-        } else if (no marking present && rc.canMarkResourcePattern(rc.getLocation())){
-
-
-            mark this area?
         }
+        // else if (no marking present && rc.canMarkResourcePattern(rc.getLocation())){
 
-                no marking present and we can mark this area) {
+        // mark this area?
+        // }
 
-            mark resource pattern
-        }
+        // no marking present and we can mark this area) {
 
-        if (marked squares nearby) {
-            go towards where you want to paint and/or try to paint it
-        } else {
-            attack enemy tower and/or navigate to some area
-        }
+        // mark resource pattern
+        // }
 
+        // if (marked squares nearby) {
+        // go towards where you want to paint and/or try to paint it
+        // } else {
+        // attack enemy tower and/or navigate to some area
+        // }
 
-
-//        if (we can paint a marked tile) {
-//            paint it
-//        } else if (we can move toward a marked tile that we can paint) {
-//            move towards it and then paint
-//        } else if (we can mark a tower pattern) {
-//            mark it
-//        } else if (we can mark a resource pattern [that isnt already marked]) {
-//            mark it
-//        }
-
+        // if (we can paint a marked tile) {
+        // paint it
+        // } else if (we can move toward a marked tile that we can paint) {
+        // move towards it and then paint
+        // } else if (we can mark a tower pattern) {
+        // mark it
+        // } else if (we can mark a resource pattern [that isnt already marked]) {
+        // mark it
+        // }
 
         // Search for a nearby ruin to complete.
-        MapInfo curRuin = null;
-        for (MapInfo tile : nearbyTiles){
-            if (tile.hasRuin()){
+        // MapInfo curRuin = null;
+        for (MapInfo tile : nearbyTiles) {
+            if (tile.hasRuin()) {
                 curRuin = tile;
             }
         }
-        if (curRuin != null){
+        if (curRuin != null) {
             MapLocation targetLoc = curRuin.getMapLocation();
             Direction dir = rc.getLocation().directionTo(targetLoc);
             if (rc.canMove(dir))
                 rc.move(dir);
             // Mark the pattern we need to draw to build a tower here if we haven't already.
             MapLocation shouldBeMarked = curRuin.getMapLocation().subtract(dir);
-            if (rc.senseMapInfo(shouldBeMarked).getMark() == PaintType.EMPTY && rc.canMarkTowerPattern(UnitType.LEVEL_ONE_PAINT_TOWER, targetLoc)){
+            if (rc.senseMapInfo(shouldBeMarked).getMark() == PaintType.EMPTY
+                    && rc.canMarkTowerPattern(UnitType.LEVEL_ONE_PAINT_TOWER, targetLoc)) {
                 rc.markTowerPattern(UnitType.LEVEL_ONE_PAINT_TOWER, targetLoc);
                 System.out.println("Trying to build a tower at " + targetLoc);
             }
             // Fill in any spots in the pattern with the appropriate paint.
-            for (MapInfo patternTile : rc.senseNearbyMapInfos(targetLoc, 8)){
-                if (patternTile.getMark() != patternTile.getPaint() && patternTile.getMark() != PaintType.EMPTY){
+            for (MapInfo patternTile : rc.senseNearbyMapInfos(targetLoc, 8)) {
+                if (patternTile.getMark() != patternTile.getPaint() && patternTile.getMark() != PaintType.EMPTY) {
                     boolean useSecondaryColor = patternTile.getMark() == PaintType.ALLY_SECONDARY;
                     if (rc.canAttack(patternTile.getMapLocation()))
                         rc.attack(patternTile.getMapLocation(), useSecondaryColor);
                 }
             }
             // Complete the ruin if we can.
-            if (rc.canCompleteTowerPattern(UnitType.LEVEL_ONE_PAINT_TOWER, targetLoc)){
+            if (rc.canCompleteTowerPattern(UnitType.LEVEL_ONE_PAINT_TOWER, targetLoc)) {
                 rc.completeTowerPattern(UnitType.LEVEL_ONE_PAINT_TOWER, targetLoc);
                 rc.setTimelineMarker("Tower built", 0, 255, 0);
                 System.out.println("Built a tower at " + targetLoc + "!");
@@ -111,13 +109,13 @@ public class Soldier extends Bunny {
         // Move and attack randomly if no objective.
         Direction dir = directions[rng.nextInt(directions.length)];
         MapLocation nextLoc = rc.getLocation().add(dir);
-        if (rc.canMove(dir)){
+        if (rc.canMove(dir)) {
             rc.move(dir);
         }
         // Try to paint beneath us as we walk to avoid paint penalties.
         // Avoiding wasting paint by re-painting our own tiles.
         MapInfo currentTile = rc.senseMapInfo(rc.getLocation());
-        if (!currentTile.getPaint().isAlly() && rc.canAttack(rc.getLocation())){
+        if (!currentTile.getPaint().isAlly() && rc.canAttack(rc.getLocation())) {
             rc.attack(rc.getLocation());
         }
 
