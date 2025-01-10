61e010d2e9ea5f3f5f498949d9264c818ed06d48
Reevu Adakroy <reevu.adakroy@gmail.com>
2025-01-10 11:53:30 -0500

    Fixed tower attack bug

---
+++ src/boostedboony/BunnyComms.java
@@ -0,0 +1,9 @@
+package boostedboony;
+
+import battlecode.common.RobotController;
+
+public class BunnyComms extends Comms {
+    public BunnyComms(RobotController rc, Robot robot) {
+        super(rc, robot);
+    }
+}
