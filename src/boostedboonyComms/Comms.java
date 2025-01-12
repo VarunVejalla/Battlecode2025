9f78c33d4649288a86cba19992bba7422ca67732
Saahith Janapati <itzsaahith01@gmail.com>
2025-01-11 23:18:57 -0500

    scanSector method and helpers

--- src/boostedboonyComms/Comms.java
+++ src/boostedboonyComms/Comms.java
@@ -1,8 +1,8 @@
 package boostedboonyComms;
 
 import battlecode.common.MapLocation;
-import battlecode.common.Message;
 import battlecode.common.RobotController;
+import battlecode.common.*;
 
 public class Comms {
 
@@ -18,11 +18,14 @@ public class Comms {
     public Comms(RobotController rc) {
         this.rc = rc;
 
-        sectorRows = (rc.getMapWidth() + 4) / 5;
-        sectorCols = (rc.getMapHeight() + 4) / 5;
-        sectorCount = sectorCols * sectorRows;
-        myWorld = new int[sectorCount];
+        this.sectorRows = (rc.getMapWidth() + 4) / 5;
+        this.sectorCols = (rc.getMapHeight() + 4) / 5;
+        this.sectorCount = sectorCols * sectorRows;
+        this.myWorld = new int[sectorCount];
     }
+
+
+
 
     /**
      * Converts tile counts into 2-bit representation:
@@ -71,7 +74,7 @@ public class Comms {
         int emptyCount = (sectorValue >> 4) & 0b11;
         int enemyPaintCount = (sectorValue >> 6) & 0b11;
 
-        return new int[] {enemyPaintCount, emptyCount, ruinCondition, staleBit};
+        return new int[]{enemyPaintCount, emptyCount, ruinCondition, staleBit};
     }
 
     /**
@@ -96,6 +99,7 @@ public class Comms {
         return row * sectorCols + col;
     }
 
+
     /**
      * Returns the index of the sector that is fully contained within a vision radius from center.
      * Returns -1 if there is no such sector.
@@ -105,11 +109,132 @@ public class Comms {
         int sectorIndex = getSectorIndex(center);
 
         // If center is within radius squared 4 of the sector center, the sector is fully visible, even if the sector is cutoff!
-        if(center.isWithinDistanceSquared(getSectorCenter(sectorIndex), 4)) {
+        if (center.isWithinDistanceSquared(getSectorCenter(sectorIndex), 4)) {
             return sectorIndex;
         }
 
         return -1;
     }
 
+
+    /**
+     * Scans a sector and returns the scan result containing tower type, enemy paint count, and empty paint count.
+     */
+    public ScanResult scanSector(int sectorIndex) throws GameActionException {
+        int towerType = 0; // Default value
+        int enemyPaintCount = 0;
+        int emptyPaintCount = 0;
+
+        MapLocation sectorCenter = getSectorCenter(sectorIndex);
+        // Determine the bottom-left corner of the sector
+        int startX = sectorCenter.x - 2;
+        int startY = sectorCenter.y - 2;
+
+        for (int dx = 0; dx < 5; dx++) {
+            for (int dy = 0; dy < 5; dy++) {
+                int x = startX + dx;
+                int y = startY + dy;
+
+                MapLocation scanLoc = new MapLocation(x, y);
+                if (!rc.onTheMap(scanLoc) || !rc.canSenseLocation(scanLoc)) {
+                    continue; // Skip out-of-bounds locations
+                }
+
+                MapInfo mapInfo = rc.senseMapInfo(scanLoc);
+
+
+                if (towerType == 0) { // only Check for tower if we have not already found one
+
+                    if (mapInfo.hasRuin()) {
+                        towerType = 1; //set tower type to one if it has a ruin
+                    } else if (rc.canSenseRobotAtLocation(scanLoc)) {
+                        RobotInfo robot = rc.senseRobotAtLocation(scanLoc);
+                        if (Util.isTower(robot.type)) {
+                            // Determine tower type based on additional properties
+                            towerType = determineTowerType(robot);
+                        }
+                    }
+                }
+
+                if (mapInfo.getPaint() == PaintType.ENEMY_PRIMARY || mapInfo.getPaint() == PaintType.ENEMY_SECONDARY) {
+                    enemyPaintCount++;
+                } else if (mapInfo.getPaint() == PaintType.EMPTY) {
+                    emptyPaintCount++;
+                }
+            }
+        }
+
+        return new ScanResult(towerType,
+                convertPaintCountToLevel(enemyPaintCount),
+                convertPaintCountToLevel(emptyPaintCount));
+    }
+
+
+    /**
+     * Determines the tower type mapping
+     * 0	none
+     * 1	ruin
+     *
+     * (these two cases are handled in the method above)
+     *
+     * 2	f - paint
+     * 3	f - money
+     * 4	f - defense
+     * 5	e - paint
+     * 6	e - money
+     * 7	e - defense
+     */
+    private int determineTowerType(RobotInfo robot) {
+        if (robot.getTeam() == rc.getTeam()) {
+            switch (robot.type) {
+                case LEVEL_ONE_PAINT_TOWER:
+                case LEVEL_TWO_PAINT_TOWER:
+                case LEVEL_THREE_PAINT_TOWER:
+                    return 2;
+                case LEVEL_ONE_MONEY_TOWER:
+                case LEVEL_TWO_MONEY_TOWER:
+                case LEVEL_THREE_MONEY_TOWER:
+                    return 3;
+                case LEVEL_ONE_DEFENSE_TOWER:
+                case LEVEL_TWO_DEFENSE_TOWER:
+                case LEVEL_THREE_DEFENSE_TOWER:
+                    return 4;
+            }
+        } else {
+            switch (robot.type) {
+                case LEVEL_ONE_PAINT_TOWER:
+                case LEVEL_TWO_PAINT_TOWER:
+                case LEVEL_THREE_PAINT_TOWER:
+                    return 5;
+                case LEVEL_ONE_MONEY_TOWER:
+                case LEVEL_TWO_MONEY_TOWER:
+                case LEVEL_THREE_MONEY_TOWER:
+                    return 6;
+                case LEVEL_ONE_DEFENSE_TOWER:
+                case LEVEL_TWO_DEFENSE_TOWER:
+                case LEVEL_THREE_DEFENSE_TOWER:
+                    return 7;
+            }
+        }
+        return 0; // Default case, should not happen
+    }
+
+    public int convertPaintCountToLevel(int paintCount) {
+        if (paintCount < 2) return 0;
+        if (paintCount < 5) return 1;
+        if (paintCount <= 12) return 2;
+        return 3;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/boostedboonyComms/ScanResult.java
@@ -0,0 +1,16 @@
+package boostedboonyComms;
+
+/**
+ * Basic datastructure to hold scan results that will be used to populate comms
+ */
+public class ScanResult {
+    public final int towerType;
+    public final int enemyPaintCount;
+    public final int emptyPaintCount;
+
+    public ScanResult(int towerType, int enemyPaintCount, int emptyPaintCount) {
+        this.towerType = towerType;
+        this.enemyPaintCount = enemyPaintCount;
+        this.emptyPaintCount = emptyPaintCount;
+    }
+}
