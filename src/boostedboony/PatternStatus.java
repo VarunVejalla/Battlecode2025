57f37f2a12d0bc687c6a6f2019d9529443ba5ba6
Varun Vejalla <varun.vejalla@gmail.com>
2025-01-13 00:40:11 -0500

    assume walls on the outside - helpful for checking for patterns and utils. guarantees length-69 map info

---
+++ src/boostedboony/PatternStatus.java
@@ -0,0 +1,13 @@
+package boostedboony;
+
+public enum PatternStatus {
+    INVALID,
+    FINISHED,
+    POSSIBLY_FINISHED,
+    BLOCKED_BY_ENEMY,
+    BLOCKED_BY_SELF,
+    BLOCKED_BY_NOTHING;
+}
+
+
+// if
