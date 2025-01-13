4fc47b9458bc418e8584bbc60567436b95e002ba
Srikar <srikarg89@gmail.com>
2025-01-13 12:15:11 -0500

    Make blitz2 a copy of commedupboony

---
+++ src/blitz2/PaintTower.java
@@ -0,0 +1,14 @@
+package blitz2;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public class PaintTower extends Tower {
+    public PaintTower(RobotController rc) throws GameActionException {
+        super(rc);
+    }
+
+    public void run() throws GameActionException {
+        super.run();
+    }
+}
