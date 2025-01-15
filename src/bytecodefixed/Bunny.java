9b4ccddb8de782856c95dba4971ad7a8bc01a772
Srikar Gouru <srikarg89@gmail.com>
2025-01-15 15:35:04 -0500

    Bytecode fixed copy

---
+++ src/bytecodefixed/Bunny.java
@@ -0,0 +1,337 @@
+package bytecodefixed;
+
+import battlecode.common.*;
+
+enum TowerType {
+    PaintTower, MoneyTower, DefenseTower;
+
+    public static TowerType from(UnitType unitType){
+        if(Util.isPaintTower(unitType)){
+            return PaintTower;
+        }
+        if(Util.isMoneyTower(unitType)){
+            return MoneyTower;
+        }
+        return DefenseTower;
+    }
+}
+
+enum SymmetryType {
+    HORIZONTAL,
+    VERTICAL,
+    ROTATIONAL,
+    DIAGONAL_RIGHT,
+    DIAGONAL_LEFT
+}
+
+public abstract class Bunny extends Robot {
+    MapLocation[] knownAlliedTowerLocs = new MapLocation[10];
+    TowerType[] knownAlliedTowerTypes = new TowerType[10];
+    TowerType nearestAlliedTowerType;
+    MapLocation nearestAlliedPaintTowerLoc;
+    MapLocation destination; // long-term destination
+    MapInfo[] nearbyMapInfos;
+    RobotInfo[] nearbyFriendlies;
+    RobotInfo[] nearbyOpponents;
+    boolean tryingToReplenish = false;
+    BunnyComms comms = new BunnyComms(rc, this);
+    SymmetryType[] possibleSymmetries = {SymmetryType.HORIZONTAL, SymmetryType.VERTICAL, SymmetryType.ROTATIONAL, SymmetryType.DIAGONAL_LEFT, SymmetryType.DIAGONAL_RIGHT};
+
+    public Bunny(RobotController rc) throws GameActionException {
+        super(rc);
+        Util.logBytecode("Bunny constructor");
+        MarkingUtils.bunny = this;
+        MarkingUtils.rc = rc;
+        destination = Util.getRandomMapLocation();
+        if(this.mapHeight != this.mapWidth){
+            for(int i = 0; i < possibleSymmetries.length; i++){
+                if(possibleSymmetries[i] == SymmetryType.DIAGONAL_LEFT || possibleSymmetries[i] == SymmetryType.DIAGONAL_RIGHT){
+                    possibleSymmetries[i] = null;
+                }
+            }
+        }
+    }
+
+    public void run() throws GameActionException {
+        super.run();
+        // Comms is run inside of scan surroundings (and nearest allied paint tower, which is called in surroundings)!
+        Util.logBytecode("Before scanning surroundings");
+        scanSurroundings();
+        Util.logBytecode("Scanned surroundings");
+
+        // If waiting for a map, stay in place. Otherwise, move!
+        if(comms.waitingForMap){ // don't move if we're waiting to receive a map from a tower
+            Util.log("Bunny @ " + rc.getLocation() + ". Pausing movement because I'm waiting for a map!");
+        }
+    }
+
+    public boolean canMove() {
+        return rc.isMovementReady(); //&& !comms.waitingForMap;
+    }
+
+    /**
+     * Evalute the sectors that are neighboring your current sector and move towards the best one.
+     */
+    // TODO: There are bugs in this method. This needs to be checked and not used yet.
+    public void macroMove() throws GameActionException {
+        int bestScore = Integer.MIN_VALUE;
+        int bestSector = -1;
+        int[] neighborSectorIndexes = comms.getSectorAndNeighbors(myLoc);
+        int sectorScore;
+
+        for (int neighorSectorIndex : neighborSectorIndexes) {
+            sectorScore = evaluateSector(comms.myWorld[neighorSectorIndex]);
+            if(sectorScore > bestScore) {
+                bestScore = sectorScore;
+                bestSector = neighorSectorIndex;
+            }
+        }
+
+        // Go to the center of that sector.
+        nav.goTo(comms.getSectorCenter(bestSector), 0);
+    }
+
+    /**
+     * Evalute the encoded information about each sector depending on the specific Bunny implementation.
+     * Returns an int score. Higher scores are considered better.
+     */
+    public abstract int evaluateSector(int encodedSector) throws GameActionException;
+
+    /**
+     * Scan stuff around you (this method is executed at the beginning of every
+     * turn)
+     */
+    public void scanSurroundings() throws GameActionException {
+        Util.logBytecode("Before filling in map info");
+        nearbyMapInfos = Util.getFilledInMapInfo(rc.senseNearbyMapInfos());
+        Util.logBytecode("After filling in map info");
+        nearbyFriendlies = rc.senseNearbyRobots(-1, rc.getTeam());
+        nearbyOpponents = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
+
+        // COMMS IS HERE
+        // Find sector that is fully enclosed and update bunny world.
+        Util.logBytecode("Before updating comms");
+        comms.updateSectorInVision(rc.getLocation());
+        Util.logBytecode("After updating comms");
+
+        // If you requested a map, wait for the tower to send it.
+        if(comms.waitingForMap) {
+            comms.processMap();
+        } else if (comms.waitingForMap2) {
+            comms.processMap2();
+        }
+
+        Util.logBytecode("After processing maps");
+
+        // Updates both nearest allied paint tower and nearest allied tower.
+        updateKnownTowers();
+        setNearestAlliedTowers();
+        Util.logBytecode("After updating tower info");
+    }
+
+    public void updateKnownTowers() throws GameActionException {
+        // Filter out old tower locations.
+        for (int i = 0; i < knownAlliedTowerLocs.length; i++) {
+            if (knownAlliedTowerLocs[i] == null) {
+                continue;
+            }
+            if (rc.canSenseLocation(knownAlliedTowerLocs[i])) {
+                RobotInfo info = rc.senseRobotAtLocation(knownAlliedTowerLocs[i]);
+                if (info == null || info.getTeam() != myTeam) {
+                    knownAlliedTowerLocs[i] = null;
+                    knownAlliedTowerTypes[i] = null;
+                } else {
+                    knownAlliedTowerTypes[i] = TowerType.from(info.getType());
+                }
+            }
+        }
+
+        // Add in any new locations.
+        for (RobotInfo bot : nearbyFriendlies) {
+            if (!Util.isTower(bot.getType())) {
+                continue;
+            }
+
+            MapLocation currAlliedTowerLocation = bot.getLocation();
+
+            // TODO This is wasteful. We request a map multiple times even if ours is currently being serviced.
+            // Always sendMessages if you're in range of a tower. The sendMessages method assesses what message to send.
+
+            // Util.log("Bunny " + rc.getID() + " at " + rc.getLocation() + " found a tower nearby " + bot.getLocation());
+            comms.sendMessages(bot);
+
+            boolean alreadyIn = false;
+            int nullIdx = -1;
+            for (int i = 0; i < knownAlliedTowerLocs.length; i++) {
+                if (knownAlliedTowerLocs[i] == null) {
+                    nullIdx = i;
+                    continue;
+                }
+                if (currAlliedTowerLocation.equals(knownAlliedTowerLocs[i])) {
+                    alreadyIn = true;
+                }
+            }
+            if (!alreadyIn && nullIdx != -1) {
+                knownAlliedTowerLocs[nullIdx] = currAlliedTowerLocation;
+                knownAlliedTowerTypes[nullIdx] = TowerType.from(bot.getType());
+            }
+        }
+    }
+
+    /**
+     * Update the nearest allied tower location to replenish paint from based on
+     * surroundings
+     */
+    public void setNearestAlliedTowers() throws GameActionException {
+        // TODO: Current behavior may cause paint tower to switch between two constantly while tryna navigate to it.
+        // TODO: If that happens, then we needa hold a consistent nearestAlliedPaintTowerLoc, so the code below might help.
+        int maxDist = Integer.MAX_VALUE;
+        int maxPaintDist = Integer.MAX_VALUE;
+        nearestAlliedPaintTowerLoc = null;
+        nearestAlliedTowerType = null;
+        MapLocation myLocation = rc.getLocation();
+        for(int i = 0; i < knownAlliedTowerLocs.length; i++){
+            if(knownAlliedTowerLocs[i] == null){
+                continue;
+            }
+            int dist = myLocation.distanceSquaredTo(knownAlliedTowerLocs[i]);
+            if(dist < maxDist){
+                maxDist = dist;
+                nearestAlliedTowerType = knownAlliedTowerTypes[i];
+            }
+            if(knownAlliedTowerTypes[i] == TowerType.PaintTower && dist < maxPaintDist){
+                nearestAlliedPaintTowerLoc = knownAlliedTowerLocs[i];
+                maxPaintDist = dist;
+            }
+        }
+    }
+
+    /**
+     * Tries to replenish paint from the nearest allied tower if within range to
+     * transfer paint
+     */
+    public void tryReplenish() throws GameActionException {
+//        if (nearestAlliedPaintTowerLoc == null) return;
+
+        if (rc.getLocation()
+                .distanceSquaredTo(nearestAlliedPaintTowerLoc) <= GameConstants.PAINT_TRANSFER_RADIUS_SQUARED) {
+            int towerPaintQuantity = rc.senseRobotAtLocation(nearestAlliedPaintTowerLoc).getPaintAmount();
+            int paintToFillUp = Math.min(
+                    rc.getType().paintCapacity - rc.getPaint(), // amount of paint needed to fully top off
+                    towerPaintQuantity); // amount of paint available in the tower
+
+            if (rc.isActionReady() && rc.canTransferPaint(nearestAlliedPaintTowerLoc, -paintToFillUp)) {
+                rc.transferPaint(nearestAlliedPaintTowerLoc, -paintToFillUp);
+            }
+
+            if (checkIfImDoneReplenishing()) {
+                // Util.log("DONE REPLENISHING");
+                tryingToReplenish = false;
+            }
+        }
+    }
+
+    /**
+     * Determine whether current you should go back to an ally tower to replenish on
+     * paint
+     * based on current paint quantity and distance to nearest tower.
+     */
+    public boolean checkIfIShouldStartReplenishing() throws GameActionException {
+        // TODO: make this a more intelligent decision based on factors like:
+        // - distance to nearest tower
+        // - whether you're really close to finishing a pattern, in which case you
+        // should consider sacrificing yourself for the greater good
+
+        return rc.getPaint() <= Constants.PAINT_THRESHOLD_TO_REPLENISH;
+    }
+
+    public boolean checkIfImDoneReplenishing() throws GameActionException {
+        // TODO: make this a more intelligent decision based on factors like:
+        // - how much paint the tower im getting it from has
+        return rc.getPaint() >= rc.getType().paintCapacity * 0.8;
+    }
+
+
+    /**
+     * Finds a ruin that is not claimed by your team.
+     */
+    public MapInfo findUnmarkedRuin() throws GameActionException {
+        // MapInfo[] nearbyTiles = rc.senseNearbyMapInfos();
+        for (MapInfo tile : nearbyMapInfos) {
+            if (rc.canSenseLocation(tile.getMapLocation()) && tile.hasRuin()) {
+                RobotInfo robotAtRuin = rc.senseRobotAtLocation(tile.getMapLocation());
+                // We want a ruin either unoccupied or not controlled by our team
+                if (robotAtRuin == null || robotAtRuin.team != rc.getTeam()) {
+                    return tile;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Update the destination we're travelling to if we need to replenish, or we've
+     * reached our current destination
+     */
+    public void updateDestinationIfNeeded() throws GameActionException {
+        if (nearestAlliedPaintTowerLoc != null && (tryingToReplenish || checkIfIShouldStartReplenishing())) {
+            destination = nearestAlliedPaintTowerLoc;
+            tryingToReplenish = true;
+            Util.addToIndicatorString("REP");
+        }
+
+        else if (destination == null ||
+                rc.getLocation().distanceSquaredTo(destination) <= Constants.MIN_DIST_TO_SATISFY_RANDOM_DESTINATION) {
+            destination = Util.getRandomMapLocation();
+        }
+    }
+
+    public MapLocation findEmptyTiles() throws GameActionException {
+        // MapInfo[] visionTiles = rc.senseNearbyMapInfos();
+        int emptyX = 0;
+        int emptyY = 0;
+        int emptyCount = 0;
+        for (MapInfo tile : nearbyMapInfos) {
+            if (tile.getPaint() == PaintType.EMPTY) {
+                emptyX += tile.getMapLocation().x;
+                emptyY += tile.getMapLocation().y;
+                emptyCount++;
+            }
+        }
+
+        if (emptyCount == 0) {
+            return null;
+        }
+
+        emptyX /= emptyCount;
+        emptyY /= emptyCount;
+
+        return new MapLocation(emptyX, emptyY);
+    }
+
+    public MapLocation findEnemyPaintCOM() throws GameActionException {
+        // MapInfo[] visionTiles = rc.senseNearbyMapInfos();
+        int enemyPaintX = 0;
+        int enemyPaintY = 0;
+        int enemyPaintCount = 0;
+        for (MapInfo tile : nearbyMapInfos) {
+            if (tile.getPaint().isEnemy()) {
+                enemyPaintX += tile.getMapLocation().x;
+                enemyPaintY += tile.getMapLocation().y;
+                enemyPaintCount++;
+            }
+        }
+
+        if (enemyPaintCount == 0) {
+            return null;
+        }
+
+        enemyPaintX /= enemyPaintCount;
+        enemyPaintY /= enemyPaintCount;
+
+        return new MapLocation(enemyPaintX, enemyPaintY);
+    }
+
+
+
+}
---
+++ src/bytecodefixed/Soldier.java
@@ -0,0 +1,300 @@
+package bytecodefixed;
+
+import battlecode.common.*;
+
+public class Soldier extends Bunny {
+
+    public static final int[] spiralOutwardIndices = {34,25,33,35,43,24,26,42,44,16,32,36,52,15,17,23,27,41,45,51,53,14,18,50,54,8,31,37,60,7,9,22,28,40,46,59,61,6,10,13,19,49,55,58,62,2,30,38,66,1,3,21,29,39,47,65,67,5,11,57,63,0,4,12,20,48,56,64,68};
+    public static final int[] shift_dx = {-4,-4,-4,-4,-4,-3,-3,-3,-3,-3,-3,-3,-2,-2,-2,-2,-2,-2,-2,-2,-2,-1,-1,-1,-1,-1,-1,-1,-1,-1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,4,4,4,4,4};
+    public static final int[] shift_dy = {-2,-1,0,1,2,-3,-2,-1,0,1,2,3,-4,-3,-2,-1,0,1,2,3,4,-4,-3,-2,-1,0,1,2,3,4,-4,-3,-2,-1,0,1,2,3,4,-4,-3,-2,-1,0,1,2,3,4,-4,-3,-2,-1,0,1,2,3,4,-3,-2,-1,0,1,2,3,-2,-1,0,1,2};
+
+    public Soldier(RobotController rc) throws GameActionException {
+        super(rc);
+        Util.logBytecode("Soldier constructor");
+        PatternUtils.soldier = this;
+        PatternUtils.rc = rc;
+        Util.logBytecode("After soldier constructor");
+    }
+
+    public void run() throws GameActionException {
+        super.run(); // Call the shared logic for all bunnies
+        updateDestinationIfNeeded();
+
+        // 1. If trying to replenish, go do that.
+        // TODO: If nearestAlliedPaintTowerLoc == null, should we explore or smth?
+        if(tryingToReplenish && nearestAlliedPaintTowerLoc != null){
+            tryReplenish();
+            if (myLoc.distanceSquaredTo(nearestAlliedPaintTowerLoc) > GameConstants.PAINT_TRANSFER_RADIUS_SQUARED) {
+                nav.goTo(nearestAlliedPaintTowerLoc, GameConstants.PAINT_TRANSFER_RADIUS_SQUARED);
+            }
+        }
+
+        else if(isAttacking()){
+            // 2. TODO: Attacking logic.
+            runAttackLogic();
+        }
+        else {
+            // 3. If not attacking, run pattern painting logic.
+            buildPattern();
+        }
+
+        MarkingUtils.tryRuinPatternCompletion();
+        MarkingUtils.tryResourcePatternCompletion();
+
+        if(canMove()) {
+            moveLogic();
+        }
+
+        // 6. End of Turn Logic
+        // Perform any shared cleanup or post-turn logic
+        sharedEndFunction();
+    }
+
+    // TODO implement this
+    public boolean isAttacking() throws GameActionException {
+        return false;
+    }
+
+    // TODO implement this
+    public void runAttackLogic() throws GameActionException {
+        return;
+    }
+
+    public void buildPattern() throws GameActionException {
+        int highPriorityRuinIndex = -1;
+        int mediumPriorityRuinIndex = -1;
+
+        UnitType intendedType = PatternUtils.getPatternUnitType();
+        boolean[][] pattern = rc.getTowerPattern(intendedType);
+
+        // Spirals outward up to vision radius.
+        // 1500 bytecode.
+        for(int index : spiralOutwardIndices) {
+            if (nearbyMapInfos[index] == null || !nearbyMapInfos[index].hasRuin() || rc.canSenseRobotAtLocation(nearbyMapInfos[index].getMapLocation())) {
+                continue;
+            }
+            PatternPriority priority = PatternUtils.findPriority(index, pattern);
+            if (priority == PatternPriority.HIGH) {
+                highPriorityRuinIndex = index;
+                break;
+            } else if (mediumPriorityRuinIndex == -1 && priority == PatternPriority.MEDIUM) {
+                mediumPriorityRuinIndex = index;
+            }
+        }
+
+        if (highPriorityRuinIndex != -1) {
+            PatternUtils.workOnRuin(highPriorityRuinIndex, pattern);
+            if (rc.canCompleteTowerPattern(intendedType, nearbyMapInfos[highPriorityRuinIndex].getMapLocation())) {
+                rc.completeTowerPattern(intendedType, nearbyMapInfos[highPriorityRuinIndex].getMapLocation());
+            }
+            return;
+        }
+
+        // 2650 bytecode.
+        int resourceCenterIndex = PatternUtils.getPotentialResourcePatternCenterIndex(nearbyMapInfos);
+
+        if (resourceCenterIndex != -1) {
+            // 300 bytecode.
+            pattern = rc.getResourcePattern();
+            PatternUtils.workOnResourcePattern(shift_dx[resourceCenterIndex], shift_dy[resourceCenterIndex], pattern);
+
+            if (rc.isMovementReady()) {
+                nav.goTo(nearbyMapInfos[resourceCenterIndex].getMapLocation(), 0);
+            }
+            if (rc.canCompleteResourcePattern(nearbyMapInfos[resourceCenterIndex].getMapLocation())) {
+                rc.completeResourcePattern(nearbyMapInfos[resourceCenterIndex].getMapLocation());
+            }
+            return;
+        }
+        if (mediumPriorityRuinIndex != -1) {
+            PatternUtils.workOnRuin(mediumPriorityRuinIndex, pattern);
+            if (rc.canCompleteTowerPattern(intendedType, nearbyMapInfos[mediumPriorityRuinIndex].getMapLocation())) {
+                rc.completeTowerPattern(intendedType, nearbyMapInfos[mediumPriorityRuinIndex].getMapLocation());
+            }
+            return;
+        }
+        PatternUtils.runDefaultBehavior();
+    }
+
+    /**
+     * Attempt to paint or attack nearby tiles if possible.
+     */
+    public void paintOrAttack() throws GameActionException {
+        MapInfo[] actionableTiles = rc.senseNearbyMapInfos(UnitType.SOLDIER.actionRadiusSquared);
+        MapLocation myLoc = rc.getLocation();
+
+        if (rc.getPaint() < Constants.MIN_PAINT_NEEDED_FOR_SOLDIER_ATTACK) {
+            return; // Not enough paint to do anything in this method
+        }
+
+        // loop over actionable enemies and try to attack them if they're a tower
+        for (RobotInfo robot : rc.senseNearbyRobots(UnitType.SOLDIER.actionRadiusSquared, rc.getTeam().opponent())) {
+            if (Util.isPaintTower(robot.getType())) {
+                if (rc.canAttack(robot.getLocation())) {
+                    rc.attack(robot.getLocation());
+                    return;
+                }
+            }
+        }
+
+        MapLocation bestPaintLoc = null;
+        int bestScore = 0;
+        boolean secondaryPaint = false;
+
+        for (MapInfo tile : actionableTiles) {
+            // Make sure tile can be painted.
+            if (!rc.canPaint(tile.getMapLocation())) {
+                continue;
+            }
+
+            // Tile score is intentionally set to 0 and not min_value.
+            int tileScore = 0;
+            // Prioritize painting marked tiles.
+            if (tile.getMark().isAlly()) {
+                // Tile is empty or wrong color.
+                boolean tileEmpty = tile.getPaint() == PaintType.EMPTY;
+                boolean wrongColor = tile.getMark().isSecondary() != tile.getPaint().isSecondary();
+                if (tileEmpty || wrongColor) {
+                    // Make sure soldier is ready and tile can be painted.
+                    if (rc.isActionReady() && rc.canPaint(tile.getMapLocation())) {
+                        // tileScore += 1000;
+                        // Attack immediately to save bytecode.
+                        rc.attack(tile.getMapLocation(), tile.getMark().isSecondary());
+                        return;
+                    }
+                }
+            }
+
+            // If there are no marked tiles, paint an empty one.
+            else if (tile.getPaint() == PaintType.EMPTY) {
+                // Reward for adjacency.
+                tileScore += 500 * adjacencyToAllyPaint(tile.getMapLocation()) + 500;
+            }
+
+            tileScore -= myLoc.distanceSquaredTo(tile.getMapLocation());
+            if (tileScore > bestScore) {
+                bestScore = tileScore;
+                bestPaintLoc = tile.getMapLocation();
+                secondaryPaint = tile.getMark().isSecondary();
+            }
+        }
+
+        // Paint the best tile that was found.
+        if (bestPaintLoc != null && rc.isActionReady()) {
+            rc.attack(bestPaintLoc, secondaryPaint);
+        }
+    }
+
+    public int adjacencyToAllyPaint(MapLocation loc) throws GameActionException {
+        Direction[] directions = Direction.allDirections();
+        int adjacentAllyTiles = 0;
+        for (Direction dir : directions) {
+            MapLocation adjacent = loc.add(dir);
+
+            // Check if the adjacent tile is within bounds and has allied paint
+            if (rc.canSenseLocation(adjacent)) {
+                MapInfo adjacentTile = rc.senseMapInfo(adjacent);
+                if (adjacentTile.getPaint().isAlly()) {
+                    adjacentAllyTiles++;
+                }
+            }
+        }
+        return adjacentAllyTiles;
+    }
+
+    /**
+     * Returns a score evaluating how favorable it would be for this robot to move to this sector.
+     */
+    public int evaluateSector(int encodedSector) {
+        ScanResult sr = comms.decodeSector(encodedSector);
+        int sectorScore = 0;
+
+        // If the region is mostly empty and there's an unbuilt ruin, go there first.
+        if(sr.emptyPaintLevel >= 2 && sr.towerType == 1) {
+            sectorScore += 1000;
+        }
+
+        // Prefer regions with 5-12 empty cells.
+        if(sr.emptyPaintLevel == 2) {
+            sectorScore += 500;
+        }
+
+        // Avoid regions with enemy cells.
+        if(sr.enemyPaintLevel >= 1) {
+            sectorScore -= 500;
+        }
+
+        return sectorScore;
+
+    }
+
+    /**
+     * Choose where to move:
+     * - If there’s an ally-marked empty tile, move toward it to paint/attack.
+     * - Otherwise move randomly.
+     */
+    public void moveLogic() throws GameActionException {
+
+
+        myLoc = rc.getLocation();
+
+        MapLocation bestDirection = null;
+        int bestScore = 0;
+
+        for (Direction dir : Direction.allDirections()) {
+            if (!rc.canMove(dir)) {
+                continue;
+            }
+            MapInfo tile = rc.senseMapInfo(myLoc.add(dir));
+            int tileScore = 0;
+
+            // Strongly favor tiles with ally color on the boundary.
+            if (isAllyBoundaryTile(tile)) {
+                tileScore += 1000;
+                // // Favor staying on your color
+                if (tile.getPaint().isAlly()) {
+                    tileScore += 5;
+                }
+            }
+
+            if (tileScore > bestScore) {
+                bestScore = tileScore;
+                bestDirection = tile.getMapLocation();
+            }
+        }
+
+        if (bestDirection != null) {
+            nav.goTo(bestDirection, 0);
+        } else {
+
+//            // Make a macro informed movement.
+//            macroMove();
+
+            // Move in a pre-determined global direction.
+            nav.goTo(destination, Constants.MIN_DIST_TO_SATISFY_RANDOM_DESTINATION);
+        }
+    }
+
+    private boolean isAllyBoundaryTile(MapInfo tile) throws GameActionException {
+        // Make sure tile is ally.
+        if (!tile.getPaint().isAlly()) {
+            return false;
+        }
+
+        // Check for empty adjacent tiles.
+        Direction[] directions = Direction.allDirections();
+        for (Direction dir : directions) {
+            MapLocation adjacent = tile.getMapLocation().add(dir);
+            if (rc.canSenseLocation(adjacent)) {
+                MapInfo adjacentTile = rc.senseMapInfo(adjacent);
+                // Make sure is passable.
+                if (adjacentTile.isPassable()) {
+                    if (adjacentTile.getPaint() == PaintType.EMPTY) {
+                        return true; // At least one adjacent tile is empty
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
