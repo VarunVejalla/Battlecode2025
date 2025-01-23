eda8e39277118e91e83898b6e431c81fbaf76d7a
Reevu Adakroy <reevu.adakroy@gmail.com>
2025-01-22 19:34:51 -0500

    repmoney = moneymoneymoney

---
+++ src/repmoney/DefenseTower.java
@@ -0,0 +1,14 @@
+package repmoney;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public class DefenseTower extends Tower {
+    public DefenseTower(RobotController rc) throws GameActionException {
+        super(rc);
+    }
+
+    public void run() throws GameActionException {
+        super.run();
+    }
+}
